2ed0d4f4b07a04de36f6c57b1bf636176f1ee861
Katsunari2075 <katu_x1275@icloud.com>
2020-10-28 16:29:51 +0900

    新規投稿ページへの遷移と表示

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,13 @@ class RestaurantsController < ApplicationController
   def index
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
   private
   def restaurant_params
     params.require(:restaurant).permit(:store_name, :address, :genre_id, :rating_id, :description, :image).merge(user_id: current_user.id)
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :store_name
+    validates :genre_id
+    validates :rating_id
+    validates :description
+    validates :image
+  end
 end
