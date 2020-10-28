1510e933908c0cf2237e8a61cb7d1b328e907b54
Katsunari2075 <katu_x1275@icloud.com>
2020-10-28 15:36:26 +0900

    Active Storageの導入

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,9 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:store_name, :address, :genre_id, :rating_id, :description, :image).merge(user_id: current_user.id)
+  end
 end
