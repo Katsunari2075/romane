fdfc5f13053745ab82ed8f87167d35d5ec53c54a
Katsunari2075 <katu_x1275@icloud.com>
2020-10-29 16:34:00 +0900

    投稿編集機能・削除機能の実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -20,6 +20,29 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    unless current_user.id == @restaurant.user_id
+      redirect_to restaurant_path
+    end
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    restaurant = Restaurant.find(params[:id])
+    if restaurant.destroy
+      redirect_to root_path
+    end
+  end
+
   private
   def restaurant_params
     params.require(:restaurant).permit(:store_name, :address, :genre_id, :rating_id, :description, :image).merge(user_id: current_user.id)
