5682deca18ce0883fd9858a39b1879556cb9a84f
Katsunari2075 <katu_x1275@icloud.com>
2020-11-09 15:31:06 +0900

    フォロー一覧表示実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @profile = Profile.find(params[:id])
     @restaurant = @user.restaurants
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings.all
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
     post 'profiles', to: 'users/registrations#create_profile'
   end
   root to: "restaurants#index"
-  resources :users, only: :show
+  resources :users, only: [:show] do
+    get :followings, on: :member
+    get :followers, on: :member
+  end
 
   resources :restaurants do
     resources :comments, only: :create
